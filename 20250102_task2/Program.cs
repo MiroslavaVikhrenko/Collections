1018797f69331cc525e28f77e5536682aab02cae
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-02 15:13:13 -0700

    Add project files.

---
+++ 20250102_task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+
+namespace _20250102_task2
+{
+    internal class Program
+    {
+        /*
+         Создать ArrayList на 100 значений, заполнить и отсортировать в убывающем порядке.
+         */
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            int i = 0;
+
+            while (i < 100)
+            {
+                Random rnd = new Random();
+                arrayList.Add(rnd.Next(0,100));
+                i++;
+            }
+
+            Print(arrayList);
+
+            arrayList.Sort();
+            //arrayList.Reverse();
+
+            Print(arrayList);
+
+            Console.ReadKey();
+        }
+
+        public static void Print(ArrayList arrayList)
+        {
+            foreach (int i in arrayList)
+            {
+                Console.Write($"{i} |");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Collections/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        /*
+         Создать класс – Мебель, со свойствами: id, производитель, модель, цена, габариты(кортеж Ш:В:Д). 
+         Создать список класса List на 5 элементов. Используя метод в классе, вывести информацию на экран.
+         */
+        static void Main(string[] args)
+        {
+            List<Furniture> list = new List<Furniture>() 
+            {
+                new Furniture("Maker1", "Model1", 20.0m),
+                new Furniture("Maker2", "Model2", 30.0m),
+                new Furniture("Maker3", "Model3", 40.0m),
+                new Furniture("Maker4", "Model4", 50.0m),
+                new Furniture("Maker5", "Model5", 60.0m)
+            };
+
+            foreach (Furniture f in list)
+            {
+                Console.WriteLine(f.ToString());
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Furniture
+    {
+        public Guid Id { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+        public Tuple<int, int, int> Size { get; set; }
+
+        public Furniture(string maker, string model, decimal price)
+        {
+            Id = Guid.NewGuid();
+            Maker = maker;
+            Model = model;
+            Price = price;
+            Size = Tuple.Create(1, 1, 1);
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Maker} {Model}, price: {Price} CAD";
+        }
+    }
+}
