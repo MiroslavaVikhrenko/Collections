1b2a8f25891bcf11fc4728c0ec47d22e274e6324
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-08 14:34:05 -0700

    Practice linked lists

---
+++ 20250108_task11/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace _20250108_task11
+{
+    internal class Program
+    {
+        /*
+         Удалить все положительные числа из двусвязного списка.
+         */
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = PopulateList();
+            PrintLinkedList(linkedList);
+
+            RemoveAllPositiveNumbersFromLinkedList(linkedList);
+            PrintLinkedList(linkedList);
+
+            Console.ReadKey();
+        }
+
+        static LinkedList<int> PopulateList()
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                linkedList.AddLast(rnd.Next(-9, 10));
+            }
+            return linkedList;
+        }
+        static void PrintLinkedList(LinkedList<int> linkedList)
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Current linked list:");
+
+            foreach (int i in linkedList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void RemoveAllPositiveNumbersFromLinkedList(LinkedList<int> linkedList)
+        {
+            var node = linkedList.First;
+            while (node != null)
+            {
+                var nextNode = node.Next;
+                if (node.Value >= 0)
+                {
+                    linkedList.Remove(node);
+                }
+                node = nextNode;
+            }
+        }
+    }
+}
