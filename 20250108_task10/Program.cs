2601f2ec3296ba863c1b9f5846ae6c159ee5e00e
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-08 14:22:42 -0700

    Practice queues

---
+++ 20250108_task10/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _20250108_task10
+{
+    internal class Program
+    {
+        /*
+         Дана очередь, состоящая из N целых чисел. Удалите все элементы, равные первому.
+         */
+        static void Main(string[] args)
+        {
+            try
+            {
+                Queue<int> queue = new Queue<int>();
+                Random rnd = new Random();
+
+                Console.WriteLine("Enter N:");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < n; i++)
+                {
+                    queue.Enqueue(rnd.Next(0, 2));
+                }
+
+                PrintQueue(queue);
+
+                queue = DeleteElementsEqualToFirstValue(queue);
+
+                PrintQueue(queue);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        public static void PrintQueue(Queue<int> queue)
+        {
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("Current queue:");
+            int i = 1;
+            foreach (int e in queue)
+            {
+                Console.WriteLine($"{i}. {e.ToString()}");
+                i++;
+            }
+            Console.WriteLine("--------------------------------");
+        }
+
+        public static Queue<int> DeleteElementsEqualToFirstValue(Queue<int> queue)
+        {
+            int first = queue.Peek();
+            Console.WriteLine($"Deleting all values equal to the first element {first.ToString()} ");
+            queue = new Queue<int>(queue.Where(x => x != first));
+            return queue;
+
+        }
+    }
+}
